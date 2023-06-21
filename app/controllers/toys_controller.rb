7f968605326a72dbd515a36834758a770bad5bf7
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-06-21 23:23:59 +0300

    assignment done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,13 +7,14 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy,status: :updated 
   end
 
   def destroy
@@ -21,11 +22,19 @@ class ToysController < ApplicationController
     toy.destroy
     head :no_content
   end
-
+  
+  def increment_likes
+    toy=Toy.find_by(id:params[:id])
+    if toy
+      toy.update(likes:toy.likes +1)
+      render json: toy
+    else render json: {"toy aint there mehn!"}
+    end
+  end
   private
   
   def toy_params
     params.permit(:name, :image, :likes)
   end
-
+  
 end
